0eae1ecec48323ce2328359422229b1186ee13c3
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-06-21 14:26:09 -0400

    Imported auth middleware to server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-
 const { ApolloServer } = require('apollo-server-express');
+
+// bring in typeDefs and resolvers for Apollo server
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+// connect to database
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// context will go here for auth later
+// configure apollo server, import auth middleware for JWT's
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
